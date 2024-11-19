9ec892d41e6ced5eb8d8c310b0bef3ee6f451729
addey34 <adrian34470@gmail.com>
2024-11-19 18:31:02 +0100

    fix function

--- src/components/CalculatorApp.vue
+++ src/components/CalculatorApp.vue
@@ -11,7 +11,7 @@
           class="toggle"
         />
         <CalcButton
-          :label="currentDisplay === '0' ? 'AC' : 'C'"
+          :label="currentDisplay === '0' || currentDisplay === '' ? 'AC' : 'C'"
           @button-click="clear"
           isClear
         />
@@ -105,7 +105,11 @@
           @button-click="calculateScientific('arctan')"
           isSpecial
         />
-        <CalcButton label="AC" @button-click="clear" isClear />
+        <CalcButton
+          :label="currentDisplay === '0' || currentDisplay === '' ? 'AC' : 'C'"
+          @button-click="clear"
+          isClear
+        />
         <CalcButton
           label="+/-"
           @button-click="handleOperator('+/-')"
@@ -116,7 +120,7 @@
         <CalcButton label="7" @button-click="handleInput('7')" />
         <CalcButton label="8" @button-click="handleInput('8')" />
         <CalcButton label="9" @button-click="handleInput('9')" />
-        <CalcButton label="x" @button-click="handleOperator('*')" isOperator />
+        <CalcButton label="x" @button-click="handleOperator('x')" isOperator />
         <CalcButton label="4" @button-click="handleInput('4')" />
         <CalcButton label="5" @button-click="handleInput('5')" />
         <CalcButton label="6" @button-click="handleInput('6')" />
@@ -157,78 +161,220 @@ export default {
     };
   },
   methods: {
-    toggleMode() {
-      this.isScientificMode = !this.isScientificMode;
-    },
     handleInput(value) {
+      // Si la dernière entrée est une constante et que l'on tape un chiffre
+      if (/[eπ]$/.test(this.currentDisplay) && /[0-9]$/.test(value)) {
+        this.currentDisplay += 'x' + value;
+        this.expression += 'x' + value;
+        return;
+      }
+
+      // Si la dernière entrée est un nombre et que l'on tape une constante, ajoute 'x' entre le nombre et la constante
+      if (/[0-9]$/.test(this.currentDisplay) && /[eπ]$/.test(value)) {
+        this.currentDisplay += 'x' + value;
+        this.expression += 'x' + this.constants[value]; // Remplace par la valeur de la constante
+        return;
+      }
+
+      // Si la dernière entrée est une parenthèse fermante et qu'on tape un chiffre, on ajoute 'x' entre ) et le chiffre
+      if (/\)$/.test(this.currentDisplay) && /[0-9]$/.test(value)) {
+        this.currentDisplay += 'x' + value;
+        this.expression += 'x' + value;
+        return;
+      }
+
+      // Si la dernière entrée est un opérateur, et qu'on entre un autre opérateur, remplace par le dernier entrée
+      if (/[+\-x/^%]$/.test(this.currentDisplay) && /[+\-x/^%]/.test(value)) {
+        this.currentDisplay = this.currentDisplay.slice(0, -1) + value;
+        this.expression = this.expression.slice(0, -1) + value;
+        return;
+      }
+
+      // Si l'entrée est une parenthèse ouvrante '(', on l'ajoute sans conditions
+      if (value === '(') {
+        this.currentDisplay += value;
+        this.expression += value;
+        return;
+      }
+
+      // Si l'entrée est une parenthèse fermante ')', on l'ajoute sans conditions
+      if (value === ')') {
+        this.currentDisplay += value;
+        this.expression += value;
+        return;
+      }
+
+      // Sinon, on ajoute la valeur à l'affichage et à l'expression
       this.currentDisplay += value;
       this.expression += value;
     },
-    handleOperator(operator) {
-      if (this.currentDisplay === '' && operator !== '-') return;
-      const operatorToUse = operator === 'x' ? '*' : operator;
-      this.currentDisplay += operator === 'x' ? 'x' : operator;
-      this.expression += operatorToUse;
+
+    handleConstant(constant) {
+      if (/[eπ]$/.test(this.currentDisplay)) {
+        this.currentDisplay += 'x' + constant;
+        this.expression += '*' + this.constants[constant];
+        return;
+      }
+
+      if (this.currentDisplay && /[0-9)]$/.test(this.currentDisplay)) {
+        this.currentDisplay += 'x';
+        this.expression += '*';
+      }
+      this.currentDisplay += constant;
+      this.expression += this.constants[constant];
     },
+
+    handleOperator(operator) {
+      if (operator === '+/-' && this.currentDisplay) {
+        if (!isNaN(parseFloat(this.currentDisplay))) {
+          this.currentDisplay = (
+            parseFloat(this.currentDisplay) * -1
+          ).toString();
+          this.expression = this.currentDisplay;
+        }
+        return;
+      }
+
+      if (operator === '*') {
+        operator = 'x';
+      }
+      this.currentDisplay += operator;
+      this.expression += operator;
+    },
+
+    startExponentiation() {
+      this.currentDisplay += '^';
+      this.expression += '^';
+    },
+
+    calculateScientific(func) {
+      if (this.currentDisplay === '' || this.currentDisplay.endsWith('(')) {
+        // Si l'affichage est vide ou se termine par une parenthèse ouverte,
+        // on ajoute simplement la fonction avec une parenthèse ouverte
+        this.currentDisplay += `${func}(`;
+        this.expression += `${func}(`;
+      } else {
+        // Sinon, on calcule le résultat
+        let angle = parseFloat(this.currentDisplay);
+        if (!isNaN(angle)) {
+          let result;
+          switch (func) {
+            case 'sin':
+              result = Math.sin((angle * Math.PI) / 180);
+              break;
+            case 'cos':
+              result = Math.cos((angle * Math.PI) / 180);
+              break;
+            case 'tan':
+              result = Math.tan((angle * Math.PI) / 180);
+              break;
+            case 'arcsin':
+            case 'asin':
+              result = (Math.asin(angle) * 180) / Math.PI;
+              break;
+            case 'arccos':
+            case 'acos':
+              result = (Math.acos(angle) * 180) / Math.PI;
+              break;
+            case 'arctan':
+            case 'atan':
+              result = (Math.atan(angle) * 180) / Math.PI;
+              break;
+            case 'log':
+              result = Math.log10(angle);
+              break;
+            case 'ln':
+              result = Math.log(angle);
+              break;
+            case 'sqrt':
+              result = Math.sqrt(angle);
+              break;
+            case 'pow':
+              result = Math.pow(angle, 2);
+              break;
+            case 'exp':
+              result = Math.exp(angle);
+              break;
+            default:
+              result = NaN;
+          }
+          this.currentDisplay = result.toString();
+          this.expression = result.toString();
+        }
+      }
+    },
+
     deleteLastCharacter() {
       this.currentDisplay = this.currentDisplay.slice(0, -1);
       this.expression = this.expression.slice(0, -1);
     },
+
     clear() {
       this.currentDisplay = '';
       this.expression = '';
     },
+
     compute() {
       try {
-        const result = evaluate(this.expression);
-        this.history.push(`${this.expression} = ${result}`);
+        let sanitizedExpression = this.expression
+          .replace(/x/g, '*')
+          .replace(/π/g, Math.PI)
+          .replace(/e/g, Math.E);
+
+        // Convertir les angles en radians pour les fonctions trigonométriques
+        sanitizedExpression = sanitizedExpression.replace(
+          /(sin|cos|tan)\(([^)]+)\)/g,
+          (match, func, angle) => `${func}((${angle}) * Math.PI / 180)`
+        );
+
+        // Gérer les fonctions trigonométriques inverses
+        sanitizedExpression = sanitizedExpression.replace(
+          /(arcsin|arccos|arctan)\(([^)]+)\)/g,
+          (match, func, value) => {
+            const mathFunc = func.replace('arc', 'a');
+            return `(Math.${mathFunc}(${value}) * 180 / Math.PI)`;
+          }
+        );
+
+        let result = evaluate(sanitizedExpression);
+
+        // Arrondir le résultat à un nombre raisonnable de décimales
+        result = Number(result.toFixed(10));
+
         this.currentDisplay = result.toString();
         this.expression = result.toString();
-      } catch {
+        this.history.push(`${this.expression} = ${result}`);
+      } catch (error) {
+        console.error('Erreur de calcul:', error);
         this.currentDisplay = 'Erreur';
+        this.expression = '';
       }
     },
+
+    retainFocus() {
+      this.$refs.calculator.focus();
+    },
+
     handleKeyPress(event) {
-      if (this.$refs.calculator !== document.activeElement) return;
       const key = event.key;
-
-      if (this.$refs.calculator !== document.activeElement) return;
-
-      if (!isNaN(parseInt(key))) {
+      if (/[0-9+\-x/^%.()]/.test(key)) {
         this.handleInput(key);
-      }
-
-      if (['+', '-', 'x', '/', '%'].includes(key)) {
-        this.handleOperator(key);
-      }
-
-      if (key === 'Enter') {
+      } else if (key === 'Enter') {
         this.compute();
-      }
-
-      if (key === 'Backspace') {
+      } else if (key === 'Backspace') {
         this.deleteLastCharacter();
-      }
-
-      if (key === 'Escape') {
+      } else if (key === 'Escape') {
         this.clear();
       }
-
-      if (key === '.') {
-        this.handleInput('.');
-      }
     },
-    retainFocus() {
-      // Réactive le focus sur la calculatrice après un clic
-      this.$refs.calculator.focus();
+
+    toggleMode() {
+      this.isScientificMode = !this.isScientificMode;
     },
   },
   mounted() {
-    setTimeout(() => {
-      this.$refs.calculator.focus();
-    }, 50);
-
     window.addEventListener('keydown', this.handleKeyPress);
+    this.$refs.calculator.focus();
   },
   beforeUnmount() {
     window.removeEventListener('keydown', this.handleKeyPress);
@@ -250,7 +396,7 @@ export default {
   transition: transform 0.2s ease-in-out;
 }
 .calculator:hover {
-  transform: scale(1.02);
+  transform: scale(1.15);
 }
 .buttons {
   display: grid;
