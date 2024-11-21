53e2ccd10062849a44c420747bb068bf213e3936
addey34 <adrian34470@gmail.com>
2024-11-21 16:21:17 +0100

    fix operator and functionScientific

--- src/components/CalculatorApp.vue
+++ src/components/CalculatorApp.vue
@@ -19,20 +19,40 @@
           @button-click="handleOperator('+/-')"
           isOperator
         />
-        <CalcButton label="%" @button-click="handleOperator('%')" isOperator />
-        <CalcButton label="/" @button-click="handleOperator('/')" isOperator />
+        <CalcButton
+          label="%"
+          @button-click="handleOperator(' % ')"
+          isOperator
+        />
+        <CalcButton
+          label="/"
+          @button-click="handleOperator(' / ')"
+          isOperator
+        />
         <CalcButton label="7" @button-click="handleInput('7')" />
         <CalcButton label="8" @button-click="handleInput('8')" />
         <CalcButton label="9" @button-click="handleInput('9')" />
-        <CalcButton label="x" @button-click="handleOperator('x')" isOperator />
+        <CalcButton
+          label="x"
+          @button-click="handleOperator(' x ')"
+          isOperator
+        />
         <CalcButton label="4" @button-click="handleInput('4')" />
         <CalcButton label="5" @button-click="handleInput('5')" />
         <CalcButton label="6" @button-click="handleInput('6')" />
-        <CalcButton label="-" @button-click="handleOperator('-')" isOperator />
+        <CalcButton
+          label="-"
+          @button-click="handleOperator(' - ')"
+          isOperator
+        />
         <CalcButton label="1" @button-click="handleInput('1')" />
         <CalcButton label="2" @button-click="handleInput('2')" />
         <CalcButton label="3" @button-click="handleInput('3')" />
-        <CalcButton label="+" @button-click="handleOperator('+')" isOperator />
+        <CalcButton
+          label="+"
+          @button-click="handleOperator(' + ')"
+          isOperator
+        />
         <CalcButton label="0" @button-click="handleInput('0')" />
         <CalcButton label="." @button-click="handleInput('.')" />
         <CalcButton label="=" @button-click="compute" isEqual />
@@ -44,8 +64,8 @@
           @button-click="toggleMode"
           class="toggle"
         />
-        <CalcButton label="(" @button-click="handleInput('(')" isSpecial />
-        <CalcButton label=")" @button-click="handleInput(')')" isSpecial />
+        <CalcButton label="(" @button-click="handleInput(' ( ')" isSpecial />
+        <CalcButton label=")" @button-click="handleInput(' ) ')" isSpecial />
         <CalcButton label="π" @button-click="handleConstant('π')" isSpecial />
         <CalcButton label="e" @button-click="handleConstant('e')" isSpecial />
         <CalcButton
@@ -88,7 +108,11 @@
           @button-click="calculateScientific('exp')"
           isSpecial
         />
-        <CalcButton label="xʸ" @button-click="startExponentiation" isSpecial />
+        <CalcButton
+          label="xʸ"
+          @button-click="handleOperator(' ^ ')"
+          isSpecial
+        />
         <CalcButton
           label="arcsin"
           @button-click="calculateScientific('arcsin')"
@@ -114,20 +138,40 @@
           @button-click="handleOperator('+/-')"
           isOperator
         />
-        <CalcButton label="%" @button-click="handleOperator('%')" isOperator />
-        <CalcButton label="/" @button-click="handleOperator('/')" isOperator />
+        <CalcButton
+          label="%"
+          @button-click="handleOperator(' % ')"
+          isOperator
+        />
+        <CalcButton
+          label="/"
+          @button-click="handleOperator(' / ')"
+          isOperator
+        />
         <CalcButton label="7" @button-click="handleInput('7')" />
         <CalcButton label="8" @button-click="handleInput('8')" />
         <CalcButton label="9" @button-click="handleInput('9')" />
-        <CalcButton label="x" @button-click="handleOperator('x')" isOperator />
+        <CalcButton
+          label="x"
+          @button-click="handleOperator(' x ')"
+          isOperator
+        />
         <CalcButton label="4" @button-click="handleInput('4')" />
         <CalcButton label="5" @button-click="handleInput('5')" />
         <CalcButton label="6" @button-click="handleInput('6')" />
-        <CalcButton label="-" @button-click="handleOperator('-')" isOperator />
+        <CalcButton
+          label="-"
+          @button-click="handleOperator(' - ')"
+          isOperator
+        />
         <CalcButton label="1" @button-click="handleInput('1')" />
         <CalcButton label="2" @button-click="handleInput('2')" />
         <CalcButton label="3" @button-click="handleInput('3')" />
-        <CalcButton label="+" @button-click="handleOperator('+')" isOperator />
+        <CalcButton
+          label="+"
+          @button-click="handleOperator(' + ')"
+          isOperator
+        />
         <CalcButton label="0" @button-click="handleInput('0')" />
         <CalcButton label="." @button-click="handleInput('.')" />
         <CalcButton label="=" @button-click="compute" isEqual />
@@ -137,7 +181,6 @@
 </template>
 
 <script>
-import { evaluate } from 'mathjs';
 import CalcButton from './CalcButton.vue';
 import CalcDisplay from './CalcDisplay.vue';
 
@@ -163,74 +206,94 @@ export default {
     handleInput(value) {
       // Si la dernière entrée est une constante et que l'on tape un chiffre
       if (/[eπ]$/.test(this.currentDisplay) && /[0-9]$/.test(value)) {
-        this.currentDisplay += 'x' + value;
-        this.expression += 'x' + value;
+        this.currentDisplay += ' x ' + value;
+        this.expression += ' x ' + value;
         return;
       }
 
-      // Si la dernière entrée est un nombre et que l'on tape une constante, ajoute 'x' entre le nombre et la constante
+      // Si la dernière entrée est un nombre et que l'on tape une constante
       if (/[0-9]$/.test(this.currentDisplay) && /[eπ]$/.test(value)) {
-        this.currentDisplay += 'x' + value;
-        this.expression += 'x' + this.constants[value]; // Remplace par la valeur de la constante
+        this.currentDisplay += ' x ' + value;
+        this.expression += ' x ' + this.constants[value];
         return;
       }
 
-      // Si la dernière entrée est une parenthèse fermante et qu'on tape un chiffre, on ajoute 'x' entre ) et le chiffre
+      // Si la dernière entrée est une parenthèse fermante et qu'on tape un chiffre
       if (/\)$/.test(this.currentDisplay) && /[0-9]$/.test(value)) {
-        this.currentDisplay += 'x' + value;
-        this.expression += 'x' + value;
+        this.currentDisplay += ' x ' + value;
+        this.expression += ' x ' + value;
         return;
       }
 
-      // Si la dernière entrée est un opérateur, et qu'on entre un autre opérateur, remplace par le dernier entrée
-      if (/[+\-x/^%]$/.test(this.currentDisplay) && /[+\-x/^%]/.test(value)) {
-        this.currentDisplay = this.currentDisplay.slice(0, -1) + value;
-        this.expression = this.expression.slice(0, -1) + value;
-        return;
-      }
-
-      // Si l'entrée est une parenthèse ouvrante '(', on l'ajoute sans conditions
+      // Si l'entrée est une parenthèse ouvrante '('
       if (value === '(') {
         this.currentDisplay += value;
         this.expression += value;
         return;
       }
 
-      // Si l'entrée est une parenthèse fermante ')', on l'ajoute sans conditions
+      // Si l'entrée est une parenthèse fermante ')'
       if (value === ')') {
         this.currentDisplay += value;
         this.expression += value;
         return;
       }
 
-      // Sinon, on ajoute la valeur à l'affichage et à l'expression
       this.currentDisplay += value;
       this.expression += value;
     },
 
     handleConstant(constant) {
       if (/[eπ]$/.test(this.currentDisplay)) {
-        this.currentDisplay += 'x' + constant;
-        this.expression += '*' + this.constants[constant];
+        this.currentDisplay += ' x ' + constant;
+        this.expression += ' x ' + this.constants[constant];
         return;
       }
 
       if (this.currentDisplay && /[0-9)]$/.test(this.currentDisplay)) {
-        this.currentDisplay += 'x';
-        this.expression += '*';
+        this.currentDisplay += ' x ';
+        this.expression += ' x ';
       }
       this.currentDisplay += constant;
       this.expression += this.constants[constant];
     },
 
     handleOperator(operator) {
-      if (operator === '+/-' && this.currentDisplay) {
-        if (!isNaN(parseFloat(this.currentDisplay))) {
-          this.currentDisplay = (
-            parseFloat(this.currentDisplay) * -1
-          ).toString();
+      if (operator)
+        if (!this.currentDisplay.trim()) {
+          return;
+        }
+      if (operator === '+/-') {
+        const match = this.currentDisplay.match(
+          /([+-]?\d*\.?\d+)(?!.*[+\-x*/^%])/
+        );
+
+        if (match) {
+          const lastNumber = match[0];
+
+          const toggledNumber = lastNumber.startsWith('-')
+            ? lastNumber.slice(1)
+            : `-${lastNumber}`;
+
+          this.currentDisplay =
+            this.currentDisplay.slice(0, -lastNumber.length) + toggledNumber;
+          console.log('Nouvel affichage:', this.currentDisplay);
+
           this.expression = this.currentDisplay;
         }
+
+        return;
+      }
+
+      // Si l'avant dernière entrée est un opérateur et qu'on entre un autre opérateur
+      if (
+        /[+\-x*/^%]\s?$/.test(this.currentDisplay.trimEnd()) &&
+        /[+\-x*/^%]/.test(operator)
+      ) {
+        this.currentDisplay =
+          this.currentDisplay.slice(0, -3).trimEnd() + ' ' + operator + ' ';
+        this.expression =
+          this.expression.slice(0, -3).trimEnd() + ' ' + operator + ' ';
         return;
       }
 
@@ -241,15 +304,10 @@ export default {
       this.expression += operator;
     },
 
-    startExponentiation() {
-      this.currentDisplay += '^';
-      this.expression += '^';
-    },
-
     calculateScientific(func) {
       if (this.currentDisplay === '' || this.currentDisplay.endsWith('(')) {
-        this.currentDisplay += `${func}(`;
-        this.expression += `${func}(`;
+        this.currentDisplay += `${func}( `;
+        this.expression += `${func}( `;
       } else {
         let angle = parseFloat(this.currentDisplay);
         if (!isNaN(angle)) {
@@ -317,31 +375,45 @@ export default {
           .replace(/π/g, Math.PI)
           .replace(/e/g, Math.E);
 
+        // Gère les fonctions trigonométriques en degrés
         sanitizedExpression = sanitizedExpression.replace(
           /(sin|cos|tan)\(([^)]+)\)/g,
-          (match, func, angle) => `Math.${func}((${angle}) * Math.PI / 180)`
+          (match, func, angle) => `Math.${func}(((${angle})) * Math.PI / 180)`
         );
 
+        // Gère les fonctions trigonométriques inverses
         sanitizedExpression = sanitizedExpression.replace(
           /(arcsin|arccos|arctan)\(([^)]+)\)/g,
           (match, func, value) => {
-            const mathFunc = func.replace('arc', 'a');
+            const mathFunc = func.replace('arc', 'a'); // arcsin -> asin
             return `(Math.${mathFunc}(${value}) * 180 / Math.PI)`;
           }
         );
 
+        // Gère les autres fonctions scientifiques
         sanitizedExpression = sanitizedExpression.replace(
           /(sqrt|log|ln|pow|exp)\(([^)]+)\)/g,
           (match, func, value) => {
-            if (func === 'ln') func = 'log';
+            if (func === 'ln') {
+              func = 'log'; // ln devient log naturel (base e)
+            } else if (func === 'log') {
+              // Logarithme base 10
+              return `(Math.log(${value}) / Math.log(10))`;
+            } else if (func === 'pow') {
+              // Si pow( x ) = x^2
+              return `Math.pow(${value}, 2)`;
+            } else if (func === 'exp') {
+              // Exponentielle exp(x) => Math.exp(x)
+              return `Math.exp(${value})`;
+            }
             return `Math.${func}(${value})`;
           }
         );
 
-        let result = evaluate(sanitizedExpression);
+        // Évalue l'expression calculée
+        const result = Number(eval(sanitizedExpression).toFixed(8)); // Précision à 10 décimales
 
-        result = Number(result.toFixed(10));
-
+        // Met à jour l'affichage et l'expression
         this.currentDisplay = result.toString();
         this.expression = result.toString();
         this.history.push(`${this.expression} = ${result}`);
@@ -358,8 +430,13 @@ export default {
 
     handleKeyPress(event) {
       const key = event.key;
-      if (/[0-9+\-x/^%.()]/.test(key)) {
+
+      if (/^[+\-x/^%]$/.test(key)) {
+        this.handleOperator(` ${key} `);
+      } else if (/[0-9.]/.test(key)) {
         this.handleInput(key);
+      } else if (/[()]/.test(key)) {
+        this.handleInput(` ${key} `);
       } else if (key === 'Enter') {
         this.compute();
       } else if (key === 'Backspace') {
@@ -388,7 +465,7 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  max-width: 400px;
+  max-width: 500px;
   margin: auto;
   padding: 20px;
   background-color: #333;
@@ -397,7 +474,7 @@ export default {
   transition: transform 0.2s ease-in-out;
 }
 .calculator:hover {
-  transform: scale(1.15);
+  transform: scale(1.1);
 }
 .buttons {
   display: grid;
